5b9d230deb4a85c917dbf3b583f9a51a6329e8fe
micelagustina43-stack <micelagustina43@gmail.com>
2026-07-06 04:54:25 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+PRIMARY_MONGO = os.environ.get("PRIMARY_MONGO", "")
