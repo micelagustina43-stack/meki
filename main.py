86a8c7f9bbe3c77af2d6fc5bdb0d2c7e3fcfaa59
micelagustina43-stack <micelagustina43@gmail.com>
2026-07-06 05:37:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
+# in_memory=True memastikan tidak ada file NovusManager.session yang dibuat di folder
 bot = Client(
-    "ManagerBot",
+    "NovusManager",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
+    in_memory=True, 
     plugins=dict(root="plugins")
 )
 
 if __name__ == "__main__":
-    print("Bot sedang berjalan...")
+    print("Mulai menjalankan Novus Manager Bot (In-Memory Mode)...")
     bot.run()
