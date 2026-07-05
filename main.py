116f6e24b92f7d73703a5653179d02e60f0d07ef
micelagustina43-stack <micelagustina43@gmail.com>
2026-07-06 05:48:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+import logging
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
-# in_memory=True memastikan tidak ada file NovusManager.session yang dibuat di folder
+# Mengaktifkan log agar error terlihat di terminal
+logging.basicConfig(level=logging.INFO)
+
 bot = Client(
     "NovusManager",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    in_memory=True, 
     plugins=dict(root="plugins")
 )
 
 if __name__ == "__main__":
-    print("Mulai menjalankan Novus Manager Bot (In-Memory Mode)...")
+    print("Mulai menjalankan Novus Manager Bot...")
     bot.run()
